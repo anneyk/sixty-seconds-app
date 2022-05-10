a6d5883f415ae6723b24a6147c586f9af57f93ec
Annetkhavere <anneywan5@gmail.com>
2022-05-11 00:47:50 +0300

    move models.py file to the app folder

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from .app import db
+from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from .app import login_manager
+from . import login_manager
 from datetime import datetime
 
 @login_manager.user_loader
